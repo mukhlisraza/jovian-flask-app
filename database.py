727b264bea06e78348037026d93d6c9be1773d89
Mukhlis Raza <mukhlis.raza1998@gmail.com>
2023-06-01 21:57:20 +0500

    some final updates

--- database.py
+++ database.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.secret_key = 's3955809'
+app.secret_key = 'jovian-flask-app'
 
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'alumni-management-s3955809'
+app.config['MYSQL_DB'] = 'alumni-management'
